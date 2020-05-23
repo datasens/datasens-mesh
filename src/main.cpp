8fb0f37d1bae7ddb5f5042e4ff04d2aaec81a2e2
AndrewLaptev <nickname.avast@gmail.com>
2020-05-23 21:41:42 +0300

    Add tasks from FreeRTOS. Code structure change to app_main()

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,32 @@
 float hum, lux;
 double temp, press;
 
-void setup() {
+void TaskMoreInertSensor(void *pvParameters);
+void TaskLessInertSensors(void *pvParameters);
+extern "C" {
+    void app_main();
+}
+
+void app_main(){
+    initArduino();
     Serial.begin(115200);
     sensorsBegin();
+
+    xTaskCreate(TaskMoreInertSensor, "Lux", 2048, NULL, 1, NULL);
+    xTaskCreate(TaskLessInertSensors, "TempHumPress", 2048, NULL, 2, NULL);
 }
-void loop() {
-    getAM2320(hum);
-    getBH1750(lux);
-    getBMP180(temp, press);
-    delay(1000);
+
+void TaskMoreInertSensor(void *pvParameters){
+    while(1){
+        getBH1750(lux);
+        delay(500);
+    }
+}
+
+void TaskLessInertSensors(void *pvParameters){
+    while(1){
+        getBMP180(temp, press);
+        getAM2320(hum);
+        delay(1000);
+    }
 }
