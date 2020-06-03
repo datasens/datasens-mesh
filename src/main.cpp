58c03a22f4cdcee15b2d6f8bdb81869d3caad219
AndrewLaptev <nickname.avast@gmail.com>
2020-06-03 17:02:19 +0300

    Add time marks to MQTT messages and add sync system time over SNTP every 30 minutes.

--- src/main.cpp
+++ src/main.cpp
@@ -27,12 +27,15 @@ float hum, lux;
 double temp, press;
 
 void TaskMoreInertSensor(void *pvParameters);
-void TaskLessInertSensors(void *pvParameters);
+void TaskLessInertSensor(void *pvParameters);
 void TaskMQTTreconnect(void *pvParameters);
 void TaskPubMoreInertData(void *pvParameters);
 void TaskPubLessInertData(void *pvParameters);
+void TaskSyncTime(void *pvParameters);
 void setup_wifi();
 void MQTTreconnect();
+void syncTime();
+String addTime(double measure);
 
 extern "C" {
     void app_main();
@@ -46,21 +49,22 @@ void app_main(){
     setup_wifi();
     client.setServer(mqtt_server, 1883); // set MQTT server info
 
-    xTaskCreate(TaskMQTTreconnect, "MQTTreconnect", 2048, NULL, 1, NULL);
-    xTaskCreate(TaskMoreInertSensor, "Lux", 2048, NULL, 3, NULL);
-    xTaskCreate(TaskLessInertSensors, "TempHumPress", 2048, NULL, 3, NULL);
-    xTaskCreate(TaskPubMoreInertData, "PubMoreInertData", 2048, NULL, 2, NULL);
-    xTaskCreate(TaskPubLessInertData, "PubLessInertData", 2048, NULL, 2, NULL);
+    xTaskCreate(TaskMQTTreconnect, "MQTTreconnect", 2048, NULL, 2, NULL);
+    xTaskCreate(TaskLessInertSensor, "Lux", 2048, NULL, 3, NULL);
+    xTaskCreate(TaskMoreInertSensor, "TempHumPress", 2048, NULL, 3, NULL);
+    xTaskCreate(TaskPubLessInertData, "PubLessInertData", 2048, NULL, 3, NULL);
+    xTaskCreate(TaskPubMoreInertData, "PubMoreInertData", 2048, NULL, 3, NULL);
+    xTaskCreate(TaskSyncTime,"SyncTime", 2048, NULL, 1, NULL);
 }
 
-void TaskMoreInertSensor(void *pvParameters){
+void TaskLessInertSensor(void *pvParameters){
     while(1){
         getBH1750(lux);
         delay(500);
     }
 }
 
-void TaskLessInertSensors(void *pvParameters){
+void TaskMoreInertSensor(void *pvParameters){
     while(1){
         getBMP180(temp, press);
         getAM2320(hum);
@@ -74,20 +78,58 @@ void TaskMQTTreconnect(void *pvParameters){
     }
 }
 
-void TaskPubMoreInertData(void *pvParameters){
+void TaskPubLessInertData(void *pvParameters){
     while(1){
-        client.publish(lux_topic, String(lux).c_str(), true);
-        delay(500);
+        client.publish(lux_topic, addTime(lux).c_str(), true);
+        delay(1000);
     }
 }
 
-void TaskPubLessInertData(void *pvParameters){
+void TaskPubMoreInertData(void *pvParameters){
     while(1){
-        client.publish(temperature_topic, String(temp).c_str(), true);
-        client.publish(humidity_topic, String(hum).c_str(), true);
-        client.publish(pressure_topic, String(press).c_str(), true);
+        client.publish(temperature_topic, addTime(temp).c_str(), true);
+        client.publish(humidity_topic, addTime(hum).c_str(), true);
+        client.publish(pressure_topic, addTime(press).c_str(), true);
         delay(2000);
     }
+}
+
+void TaskSyncTime(void *pvParameters){
+    while(1){
+        syncTime();
+        delay(1000*60*30); // sync every 30 minutes
+    }
+}
+
+void MQTTreconnect(){
+    while(!client.connected()){
+        Serial.print("Attempting MQTT connection...");
+        if(client.connect("ESP32")){
+            Serial.println("connected");
+        }else{
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void syncTime(){
+    const char* ntpServer = "pool.ntp.org";
+    const long gmtOffset_sec = 10800;
+    const int daylightOffset_sec = 0;
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+String addTime(double measure){
+    struct tm timeinfo;
+    char buffer[30];
+    getLocalTime(&timeinfo);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S | ", &timeinfo);
+    String date = String(buffer);
+    String data = String(measure);
+    return date + data;
 }
 
 void setup_wifi(){
@@ -107,18 +149,4 @@ void setup_wifi(){
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-}
-
-void MQTTreconnect(){
-    while(!client.connected()){
-        Serial.print("Attempting MQTT connection...");
-        if(client.connect("ESP32")){
-            Serial.println("connected");
-        }else{
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
-        }
-    }
 }
